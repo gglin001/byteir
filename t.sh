c1a6f91c3cfad27398b7a73845c2291a4d120a7a
Allen Guo <guosonglin001@gmail.com>
2023-05-10 17:32:53 +0000

    sync llvm version

--- t.sh
+++ t.sh
@@ -1,1 +1,3 @@
+pushd external/llvm-project && git fetch && git checkout $(cat ../../build_tools/llvm_version.txt) && popd
+
 bash build_tools/build_mlir.sh ${PWD}/external/llvm-project/ ${PWD}/llvm-build
