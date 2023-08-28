5834e4f300cb6607bf3433737762b6522caec338
Allen Guo <guosonglin001@gmail.com>
2023-08-28 14:28:40 +0000

    wip

--- t.sh
+++ t.sh
@@ -1,3 +1,6 @@
 pushd external/llvm-project && git fetch && git checkout $(cat ../../build_tools/llvm_version.txt) && popd
 
 bash build_tools/build_mlir.sh ${PWD}/external/llvm-project/ ${PWD}/llvm-build
+
+ln -s $(pwd)/llvm-build $(pwd)/compiler/llvm-build
+ln -s $(pwd)/llvm-build $(pwd)/runtime/llvm-build
